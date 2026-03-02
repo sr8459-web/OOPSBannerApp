e17a1a639591e824dee6cf3e7c1aa668fe5829d1
sr8459-web <sr8459@srmist.edu.in>
2026-03-02 20:03:28 +0530

    Add files via upload

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,14 +1,7 @@
-/**
- * OOPSBannerApp UC1 OOPS Banner Display Application
- * * This class demonstrates a simple Java application that displays the Object
- * Oriented Programming System OOPS acronym to the console.
- * * @author Sukanya
- * @version 1.0
- */
-class main()
-{
-    public static void main(String []args)
-    {
-        System.out.println("Welcome to OOPSBanner App");
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("-OOPS-");
     }
 }
